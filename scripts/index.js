4ad4235dc53b92505c9e785eb42b174b796ae6b5
abdo.eslam2021@gmail.com <abdo.eslam2021@gmail.com>
2022-05-17 06:11:04 +0200

    fix phone

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,7 @@ drawer.height = body.getClientRects()[0].height - tools.getClientRects()[0].heig
 
 
 
+
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, drawer.width, drawer.height);
 
@@ -24,12 +25,15 @@ drawer.addEventListener('mousedown', start, false);
 drawer.addEventListener('mousemove', draw, false);
 
 drawer.addEventListener('touchmove', goma, false);
+drawer.addEventListener('touchend', gomaEnd, false);
 drawer.addEventListener('mousemove', goma, false);
 
-
 drawer.addEventListener('touchend', end, false);
+drawer.addEventListener('touchcancel', end, false);
 drawer.addEventListener('mouseout', end, false);
 drawer.addEventListener('mouseup', end, false);
+
+
 
 clear.addEventListener('click', (e) => {
     ctx.clearRect(0, 0, drawer.width, drawer.height);
@@ -38,35 +42,47 @@ clear.addEventListener('click', (e) => {
 
 ponsher.addEventListener('click', (e) => {
     gomer = true;
+    console.log('clicked');
     e.target.style.pointerEvents = 'none';
-    console.log('test');
 })
 
 
-inputs[0].addEventListener('change', (e) => {
+inputs[0].addEventListener('input', (e) => {
     colorData = e.target.value;
 })
-inputs[1].addEventListener('change', (e) => {
+inputs[1].addEventListener('input', (e) => {
     ctx.fillStyle = e.target.value;
     ctx.fillRect(0, 0, drawer.width, drawer.height);
     document.documentElement.style.setProperty('--backColor', e.target.value);
 })
-inputs[2].addEventListener('change', (e) => {
-    widthData = e.target.value;
+inputs[2].addEventListener('input', (e) => {
+    if (e.target.value > 100) {
+        e.target.value = '';
+        e.preventDefault();
+    } else {
+        widthData = e.target.value;
+    }
 })
-
 
 function start(e) {
     is_drawing = true;
     ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
-    e.preventDefault();
+    if (e.type == 'touchstart') {
+        ctx.moveTo(e.touches[0].clientX, e.touches[0].clientY);
+    } else {
+        ctx.moveTo(e.clientX, e.clientY);
+    }
     ponsher.style.pointerEvents = 'all';
+    e.preventDefault();
 }
 
 function draw(e) {
     if (is_drawing) {
-        ctx.lineTo(e.clientX, e.clientY)
+        if (e.type == 'touchmove') {
+            ctx.lineTo(e.touches[0].clientX, e.touches[0].clientY);
+        } else {
+            ctx.lineTo(e.clientX, e.clientY);
+        }
         ctx.strokeStyle = colorData;
         ctx.lineWidth = widthData;
         ctx.lineCap = 'round';
@@ -91,12 +107,27 @@ function end(e) {
 }
 
 function goma(e) {
-    let postionLeft = e.offsetX - (ponsher.getClientRects()[0].width / 2);
-    let postionTop = e.offsetY - (ponsher.getClientRects()[0].width / 2);
+    let postionLeft;
+    let postionTop;
+    if (e.type == 'touchmove') {
+        postionLeft = e.touches[0].clientX - (ponsher.getClientRects()[0].width / 2);
+        postionTop = e.touches[0].clientY - (ponsher.getClientRects()[0].width / 2);
+        console.log(postionLeft);
+
+    } else {
+        postionLeft = e.offsetX - (ponsher.getClientRects()[0].width / 2);
+        postionTop = e.offsetY - (ponsher.getClientRects()[0].width / 2);
+    }
     if (gomer) {
         ctx.clearRect(postionLeft, postionTop, 50, 50)
         ponsher.style.left = `${postionLeft}px`;
         ponsher.style.top = `${postionTop}px`;
         is_drawing = false;
     }
+    e.preventDefault();
+}
+
+function gomaEnd(e) {
+    gomer = false;
+    e.preventDefault();
 }
